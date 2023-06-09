77ab3d541093982f4c7ddb11c23c5b667b4778b3
MacMillon-VS <macsimson3@gmail.com>
2023-06-09 22:33:21 +0530

    2 components into single component

--- src/Components/ContentPage/ContentPage.jsx
+++ src/Components/ContentPage/ContentPage.jsx
@@ -1,5 +1,3 @@
-import RowElement from "./RowElement";
-
 const ContentPage = () => {
   const data = [
     {
@@ -37,13 +35,29 @@ const ContentPage = () => {
     
     <div className="flex-col justify-center items-center w-[90%] bg-white m-auto">
       {data.map((element,index) => (
-        <RowElement
-          key={element.id}
-          title={element.title}
-          desc={element.desc}
-          avatar={element.avatar}
-          index={index}
-        />
+        <div
+    key={index}
+    className={`flex items-center ${index % 2 !== 0 ? 'flex-row' : 'flex-row-reverse'} mt-4`}
+  >
+    <div className="flex justify-center items-center w-[300px] rounded-full bg-gray-300 border border-white shadow-[0_4px_25px_#f9a828] max-md:w-[100px] max-md:h-[100px] max-lg:w-[200px] max-lg:h-[200px]">
+    
+      <img src={element.avatar} alt={`Avatar ${index + 1}`} className="w-[300px] h-[300px] object-cover rounded-full max-md:w-[100px] max-md:h-[100px] max-lg:w-[200px] max-lg:h-[200px]" />
+    </div>
+    <div className="ml-8 w-[80%]">
+      <h3 className=" text-orange-500 font-bold mb-4 text-3xl leading-none max-md:text-[18px] max-lg:text-[20px] xl:text-[32px]">{element.title}</h3>
+      {element.desc.map((d, index) => {
+              return element.desc.length === 1 ? (
+                <p className=" text-gray-600 font-medium text-base max-md:text-[12px] max-lg:text-[16px] xl:text-[20px]" key={index}>
+                  {d}
+                </p>
+              ) : (
+                <ul key={index} className="list-disc ml-5">
+                  <li className="text-gray-600 font-medium text-base max-md:text-[12px] max-lg:text-[16px] xl:text-[20px]">{d}</li>
+                </ul>
+              );
+            })}
+        </div>
+      </div>
       ))}
     </div>
   );
