9740fb5b682427d48b0ed2053d32e914d19feeb2
MacMillon-VS <macsimson3@gmail.com>
2023-06-09 11:43:59 +0530

    version 1 first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ThankYouPage from './Components/ThankYouPage'
+
+function App() {
+
+  return (
+    <>
+     <ThankYouPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/ThankYouPage.jsx
@@ -0,0 +1,14 @@
+import "./ThankYouPage.css";
+
+const ThankYouPage = () => {
+  return (
+    <div className="thank-you-page">
+      <div className="thank-inner-box">
+        <h1 className="thank-you-heading">Thank You!</h1>
+        <p className="thank-you-message">We appreciate your submission.</p>
+      </div>
+    </div>
+  );
+};
+
+export default ThankYouPage;
